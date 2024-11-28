102de7ddb059e411719544357c01c94d55c0a6ca
THIAGO SOUZA SILVA <thiago.silva25@alunos.sc.senac.br>
2024-11-27 21:52:24 -0300

    model

---
+++ src/model/Mantimentos.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Mantimentos extends Produto {
+	
+	// criador concreto
+    public Mantimentos(String nome, double preco, int quantidade) {
+        super(nome, preco, quantidade);
+    }
+}
